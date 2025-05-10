f667c7fbc120a23880046dc38b8e6cb4ceaadb6b
ddutta <dibyendu.dutta08@gmail.com>
2025-05-10 23:12:08 +0530

    Adding a new agent to the agents directory

--- agents/Rag.py
+++ agents/Rag.py
@@ -23,9 +23,20 @@ def invoke_llm(state: "RAGState") -> RAGState:
         Define logic to invoke the LLM with the retrieved documents.
         """
         print("LLM invoked")
-def process_Resule(state: "RAGState") -> RAGState:
+def process_Result(state: "RAGState") -> RAGState:
         """
         Define logic to process the LLM response.
         """
         print("LLM response processed")
 
+workflow = StateGraph(RAGState)
+
+workflow.add_node("retrieve_similar_docs", retrieve_similar_docs)
+workflow.add_node("invoke_llm", invoke_llm)
+workflow.add_node("process_Result", process_Result)
+
+workflow.add_edge("retrieve_similar_docs", "invoke_llm")
+workflow.add_edge("invoke_llm", "process_Result")
+
+workflow.set_entry_point("retrieve_similar_docs")
+rag_graph=workflow.compile
