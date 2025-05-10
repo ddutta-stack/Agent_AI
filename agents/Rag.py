81301754ee6f3c97f070af08975c05cfc10ce2ce
ddutta <dibyendu.dutta08@gmail.com>
2025-05-10 20:57:45 +0530

    Added basic graph code

--- agents/Rag.py
+++ agents/Rag.py
@@ -9,7 +9,23 @@ class RAGState(BaseModel):
     State for the RAG agent.
     """
     # Define the state variables
+    query: Optional[str]
     retrieved_documents: Optional[List[str]]
     llm_response: Optional[str]
-    print("RAGState initialized")
-    # Define the state graph
+def retrieve_similar_docs(state: "RAGState")->RAGState:       
+        """
+        Define logic to Retrieve similar documents from the knowledge base.
+        """
+        print("RAGState initialized")
+        # Define the state graph
+def invoke_llm(state: "RAGState") -> RAGState:
+        """
+        Define logic to invoke the LLM with the retrieved documents.
+        """
+        print("LLM invoked")
+def process_Resule(state: "RAGState") -> RAGState:
+        """
+        Define logic to process the LLM response.
+        """
+        print("LLM response processed")
+
