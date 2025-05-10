7fcdd303678aa003c9bddecfd959f0a0e5260e9e
ddutta <dibyendu.dutta08@gmail.com>
2025-05-10 23:13:25 +0530

    Added more detailed docstring to Rag agent

--- knowledgebase/knowledgebase.py
+++ knowledgebase/knowledgebase.py
@@ -2,7 +2,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 # from langchain_community.vectorstores import Chroma -->Ignoring as we are not creating any expternal DB
-from langchain.vectorstores import inMemory
+from langchain.vectorstores import 
 class KnowledgeBase:
     def __init__(self, pathtopdf):
         loader = PyPDFLoader(pathtopdf)
